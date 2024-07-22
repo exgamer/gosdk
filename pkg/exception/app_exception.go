e65a50bae6292d2ee81c0bcc0391703f3972fe1b
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2024-07-22 09:59:06 +0500

    init

--- pkg/exception/app_exception.go
+++ pkg/exception/app_exception.go
@@ -9,19 +9,20 @@ import (
 
 // AppException Модель данных для описания ошибки
 type AppException struct {
-	Code    int
-	Error   error
-	Context map[string]any
+	Code        int
+	Error       error
+	Context     map[string]any
+	ServiceCode int
 }
 
-func NewAppException(code int, err error, context map[string]any) *AppException {
-	return &AppException{code, err, context}
+func NewAppException(code int, err error, context map[string]any, serviceCode int) *AppException {
+	return &AppException{code, err, context, serviceCode}
 }
 
-func NewValidationAppException(context map[string]any) *AppException {
-	return &AppException{http.StatusUnprocessableEntity, errors.New("VALIDATION ERROR"), context}
+func NewValidationAppException(context map[string]any, serviceCode int) *AppException {
+	return &AppException{http.StatusUnprocessableEntity, errors.New("VALIDATION ERROR"), context, serviceCode}
 }
 
-func NewValidationAppExceptionFromValidationErrors(validationErrors validate.Errors) *AppException {
-	return NewValidationAppException(validation.ValidationErrorsAsMap(validationErrors))
+func NewValidationAppExceptionFromValidationErrors(validationErrors validate.Errors, serviceCode int) *AppException {
+	return NewValidationAppException(validation.ValidationErrorsAsMap(validationErrors), serviceCode)
 }
